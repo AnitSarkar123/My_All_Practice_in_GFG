c45b3a34e2a24ba3f44b1bc56b524f8e90e99d0b
Anit Sarkar <anitsarkar04@gmail.com>
2024-06-25 15:05:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/nCr/n-cr.cpp
+++ Difficulty: Medium/nCr/n-cr.cpp
@@ -13,18 +13,35 @@ public:
     int nCr(int n, int r){
         // code here
        int mod=1e9+7;
-        int i,j;
-        vector<vector<int>>dp(n+1,vector<int>(r+1,0));
-        for(i=0;i<=n;i++){
-            dp[i][0]=1;
+        // int i,j;
+        // vector<vector<int>>dp(n+1,vector<int>(r+1,0));
+        // for(i=0;i<=n;i++){
+        //     dp[i][0]=1;
+        // }
+        // for(i=0;i<=n;i++){
+        //     for(j=1;j<=min(i,r);j++){
+        //         dp[i][j]=(dp[i-1][j]%mod + dp[i-1][j-1]%mod)%mod;
+        //     }
+        // }
+        // return dp[n][r];}
+        
+        //pascal triangle method
+        if(n<r){
+            return 0;
         }
-        for(i=0;i<=n;i++){
-            for(j=1;j<=min(i,r);j++){
-                dp[i][j]=(dp[i-1][j]%mod + dp[i-1][j-1]%mod)%mod;
+        if((n-r)<r){
+            r= n-r;
+        }
+        int dp[r+1];
+        memset(dp,0,sizeof(dp));
+        dp[0]=1;
+         for(int i=1;i<=n;i++){
+            for(int j=min(i,r);j>0;j--){
+                dp[j]=(dp[j] + dp[j-1])%mod;
             }
         }
-        return dp[n][r];}
-
+        return dp[r];
+}
 };
 
 //{ Driver Code Starts.
