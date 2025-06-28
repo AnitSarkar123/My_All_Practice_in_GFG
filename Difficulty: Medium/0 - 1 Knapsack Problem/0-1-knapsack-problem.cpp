a257007ca74f9fee08ce68afe06318094dea56f6
Anit Sarkar <anitsarkar04@gmail.com>
2025-06-28 12:43:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,94 +1,89 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-class Solution
-{
-    public:
-    //Function to return max value that can be put in knapsack of capacity W.
-    /*int solveRec(int Wt, int wt[], int val[], int index){
-        if(index==0){
-            if(wt[0]<= Wt){
+class Solution {
+  public:
+    int solveRec(int W, vector<int> &val, vector<int> &wt,int n){
+        if(n==0){
+            if(wt[0]<=W){
                 return val[0];
-            }else{
+            }
+            else{
                 return 0;
             }
         }
-        int include =0;
-       int exclude =0 +solveRec( Wt, wt,  val,  index-1);
-        if(wt[index]<= Wt){
-                include = val[index] +solveRec( Wt-wt[index], wt,  val,  index-1);
-            
-        }
-            
-    int ans=max(include,exclude);
-    return ans;
-    }*/
-    int solveMemo(int Wt, int wt[], int val[], int index, int dp[][1001]) { // Assuming a maximum Wt of 1000 for array size
-    if (index == 0) {
-        if (wt[0] <= Wt) {
-            return val[0];
-        } else {
-            return 0;
-        }
-    }
-    if (dp[index][Wt] != -1) {
-        return dp[index][Wt];
-    }
+        int include=0;
+        int exclude=0;
+        if(wt[n]<=W)
+            include =val[n]+solveRec(W-wt[n],val,wt,n-1);
+        
+        
+        exclude =0+solveRec(W,val,wt,n-1);
     
-    int include = 0;
-    int exclude = solveMemo(Wt, wt, val, index - 1, dp);
-    if (wt[index] <= Wt) {
-        include = val[index] + solveMemo(Wt - wt[index], wt, val, index - 1, dp);
+        int ans =max(include,exclude);
+        return ans;
     }
-    
-    dp[index][Wt] = max(include, exclude);
-    return dp[index][Wt];
-}
-
-int knapSack(int W, int wt[], int val[], int n) {
-    int dp[n][1001]; // Assuming a maximum W of 1000 for array size
-
-    // Initialize all elements to -1
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < W + 1; ++j) {
-            dp[i][j] = -1;
+    int solveMemo(int W, vector<int> &val, vector<int> &wt,int n,vector<vector<int> > &dp){
+        if(n==0){
+            if(wt[0]<=W){
+                return val[0];
+            }
+            else{
+                return 0;
+            }
         }
+        if(dp[n][W]!= -1){
+            return dp[n][W];
+        }
+        int include=0;
+        int exclude=0;
+        if(wt[n]<=W)
+            include =val[n]+solveRec(W-wt[n],val,wt,n-1);
+        
+        
+        exclude =0+solveRec(W,val,wt,n-1);
+    
+        dp[n][W] =max(include,exclude);
+        return dp[n][W];
     }
-    return solveMemo(W, wt, val, n-1, dp);
-}
+    int solveTab(int W, vector<int> &val, vector<int> &wt,int n){
+        vector<vector<int> > dp(n, vector<int>(W+1, 0));
+        
+        for(int w = wt[0]; w <= W; w++) {
+		if(wt[0] <= W)
+			dp[0][w] = val[0];
+		else
+			dp[0][w] = 0;
+	    }
+	    
+        for(int i=1;i<n;i++){
+            for(int w=0;w<=W;w++){
+                int include = 0;
+			if(wt[i] <= w)
+				include = val[i] + dp[i-1][W-wt[i]];
+			
+			int exclude = 0 + dp[i-1][w];
+
+			dp[i][w] = max(include, exclude);	
+            }
+        }
+        return dp[n-1][W];
+    }
+      
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+        // code here
+        //int n=val.size();
+        // return solveRec(W,val,wt,n);
+        // vector<vector<int> > dp(n, vector<int>(W+1, -1));
+       // return solveTab(W,val,wt,n);
+          int n = val.size();
+        vector<int>dp(W+1,0);
+        
+        for(int i = 0; i < n; i++){
+            for(int w = W; w >= wt[i]; w--){
+                dp[w] = max(dp[w], dp[w-wt[i]] + val[i]);
+            }
+        }
+        
+        return dp[W];
+    
+        
+    }
 };
-
-//{ Driver Code Starts.
-
-int main()
- {
-    //taking total testcases
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        //reading number of elements and weight
-        int n, w;
-        cin>>n>>w;
-        
-        int val[n];
-        int wt[n];
-        
-        //inserting the values
-        for(int i=0;i<n;i++)
-            cin>>val[i];
-        
-        //inserting the weights
-        for(int i=0;i<n;i++)
-            cin>>wt[i];
-        Solution ob;
-        //calling method knapSack()
-        cout<<ob.knapSack(w, wt, val, n)<<endl;
-        
-    }
-	return 0;
-}
-// } Driver Code Ends
